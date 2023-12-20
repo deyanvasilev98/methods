6633800697c927ace0c4fe5c95e449a00b32dd49
deyanvasilev98 <152769928+deyanvasilev98@users.noreply.github.com>
2023-12-20 13:22:13 +0200

    Add files via upload

---
+++ src/methods/methods.java
@@ -0,0 +1,32 @@
+package methods;
+
+public class methods {
+	
+
+public static void main(String[] args) {
+
+sayHi("Dave");
+	
+
+	
+}
+
+public static void sayHi(String name) {
+	System.out.println("Deyan"+ name);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
